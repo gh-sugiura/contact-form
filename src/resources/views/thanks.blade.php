833e3795024acbd507cd34c3e0a4586a2d65ed2d
sugiura hiroki <iamsugihiro8180@gmail.com>
2024-05-19 15:48:39 +0900

    更新

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/thanks.css')}}">   
+@endsection
+    
+@section('content')
+    <div class="title">
+        <h2 class="title__text">お問い合わせありがとうございます</h2>
+    </div>
+@endsection
